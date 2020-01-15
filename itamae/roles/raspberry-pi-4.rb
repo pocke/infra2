8461fb65b17aeb89464b5dde8d54ac7648cf65c5
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-01-16 01:46:17 +0900

    Locales

--- itamae/roles/raspberry-pi-4.rb
+++ itamae/roles/raspberry-pi-4.rb
@@ -1,7 +1,7 @@
 HOST = 'skitty'
 
 if ENV['UPDATE_ALL']
-  execute 'pacman -Syu' do
+  execute 'pacman -Syu base-devel --noconfirm' do
     user 'root'
   end
 end
@@ -29,13 +29,35 @@ end
 
 execute 'timedatectl set-timezone Asia/Tokyo' do
   user 'root'
-  not_if 'timedatectl status | grep Time zone: Asia/Tokyo'
+  not_if 'timedatectl status | grep "Time zone: Asia/Tokyo"'
+end
+
+execute 'echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen' do
+  user 'root'
+  not_if 'grep "^en_US.UTF-8 UTF-8" /etc/locale.gen'
+  notifies :run, 'execute[locale-gen]', :immediately
+end
+
+execute 'locale-gen' do
+  user 'root'
+  action :nothing
+end
+
+execute 'localectl set-locale LANG=en_US.UTF-8' do
+  user 'root'
+  not_if 'localectl status | grep "System Locale: LANG=en_US.UTF-8"'
 end
 
 # --- packages
 
-%w[vim].each do |pkg|
+%w[vim tmux].each do |pkg|
   package pkg do
     user 'root'
   end
 end
+
+# aur_package 'yay' do
+#   user 'alarm'
+# end
+# aur_package 'ruby-build'
+# aur_package 'rbenv'
